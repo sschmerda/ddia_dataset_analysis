59dcf24783f21ac1ce4ca1f16e88414f4e13b6c9
Stefan Schmerda <stefan.schmerda@gmail.com>
2023-12-17 00:36:59 +0100

    update config, constants and standard_lsimport

--- dataset_analysis/utilities/config.py
+++ dataset_analysis/utilities/config.py
@@ -1,11 +1,41 @@
 from .standard_import import *
 
+########################################################################################################################
+# sequence distance libraries
+# function needs to take 2 strings as the first 2 arguments and return a numeric distance: seq_dist(string_1, string_2)
+
+# from Levenshtein import distance as distance # second fastest implementation in python(mainly written in C)
+from polyleven import levenshtein as distance # fastest implementation in python(mainly written in C)
+
+# specify positional and keyword arguments for the distance function(if there are any)
+SEQUENCE_DISTANCE_FUNCTION_ARGS = []
+SEQUENCE_DISTANCE_FUNCTION_KWARGS = {}
+########################################################################################################################
+
+########################################################################################################################
+### parallelization options ###
+
+# specify whether the computations should be done in parallel
+PARALLELIZE_COMPUTATIONS = True
+
+# specify the number of cores to be utilized
+# all cores available will be utilized if value equals -1
+NUMBER_OF_CORES = -1
+########################################################################################################################
+
+########################################################################################################################
 ### pandas options ###
+
 pd.set_option('display.max_rows', 100)
 pd.set_option('display.max_columns', None)
+########################################################################################################################
 
+########################################################################################################################
 ### seaborn options ###
+
+# set figsize
 sns.set(rc = {'figure.figsize':(15,8)})
+
 #sns.set_context('notebook', rc={'font.size':20,'axes.titlesize':30,'axes.labelsize':20, 'xtick.labelsize':16, 'ytick.labelsize':16, 'legend.fontsize':20, 'legend.title_fontsize':20})   
 sns.set_style('darkgrid')
 marker_config = {'marker':'o',
@@ -16,6 +46,11 @@ marker_config_eval_metric_mean = {'marker':'o',
                                   'markerfacecolor':'red', 
                                   'markeredgecolor':'black',
                                   'markersize':'6'}
+########################################################################################################################
+
+########################################################################################################################
 ### numpy options ###
+
 # turn off an unecessary numpy warning
 warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning) 
+########################################################################################################################
--- dataset_analysis/utilities/standard_import.py
+++ dataset_analysis/utilities/standard_import.py
@@ -1,9 +1,11 @@
 import os
+import glob
 import time
 import warnings
 import operator
 from tqdm import tqdm
 import pandas as pd
+# import modin.pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -14,13 +16,17 @@ from scipy.stats.contingency import crosstab
 from scipy.stats import iqr
 from tqdm import tqdm
 from IPython.display import display, Markdown
+from typing import Any
 
-# textdistance library
-from Levenshtein import distance
+# sequence distance libraries
+# imported in config file!
 
-# libraries for clustering
+# loop parallelization
+from joblib import Parallel, delayed
+
+# libraries for clustering and statistical tests
 import umap
 from sklearn.decomposition import PCA
-import hdbscan
+# import hdbscan
 from scipy.spatial.distance import squareform
 import pingouin as pg
