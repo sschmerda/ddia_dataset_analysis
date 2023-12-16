29a3e2d576994673ff965a9d206956c611580404
Stefan Schmerda <stefan.schmerda@gmail.com>
2023-12-17 00:39:03 +0100

    Revert "update config, constants and standard_lsimport"
    This reverts commit 59dcf24783f21ac1ce4ca1f16e88414f4e13b6c9.

--- dataset_analysis/utilities/standard_import.py
+++ dataset_analysis/utilities/standard_import.py
@@ -1,11 +1,9 @@
 import os
-import glob
 import time
 import warnings
 import operator
 from tqdm import tqdm
 import pandas as pd
-# import modin.pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -16,17 +14,13 @@ from scipy.stats.contingency import crosstab
 from scipy.stats import iqr
 from tqdm import tqdm
 from IPython.display import display, Markdown
-from typing import Any
 
-# sequence distance libraries
-# imported in config file!
+# textdistance library
+from Levenshtein import distance
 
-# loop parallelization
-from joblib import Parallel, delayed
-
-# libraries for clustering and statistical tests
+# libraries for clustering
 import umap
 from sklearn.decomposition import PCA
-# import hdbscan
+import hdbscan
 from scipy.spatial.distance import squareform
 import pingouin as pg
