77705d1fe7d9a1e8b6d94c0864f7579e43274988
Stefan Schmerda <stefan.schmerda@gmail.com>
2024-08-30 23:32:39 +0200

    update standard_import

--- dataset_analysis/utilities/standard_import.py
+++ dataset_analysis/utilities/standard_import.py
@@ -4,18 +4,29 @@ import glob
 import time
 import random
 from collections import defaultdict
-from itertools import chain, combinations, combinations_with_replacement
+from itertools import chain, combinations, combinations_with_replacement, product
 import pickle
 import warnings
 import operator
+from abc import ABC
+from bs4 import BeautifulSoup
+from dataclasses import dataclass
+from enum import Enum
+
+# typing
 from typing import Type
 from typing import Any
 from typing import Union
 from typing import Literal
 from typing import List
+from typing import Callable
+from typing import Iterable
+from typing import Dict
+from typing import Tuple
 from numpy.typing import ArrayLike
-from abc import ABC
-from bs4 import BeautifulSoup
+from numpy.typing import NDArray
+
+# misc
 from pympler import asizeof
 
 # var
@@ -25,20 +36,31 @@ from tqdm import tqdm
 # stats libraries
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.markers import MarkerStyle
-import seaborn as sns
-from seaborn.axisgrid import FacetGrid
 import statsmodels.api as sm
 from scipy.stats import iqr
-from scipy.stats.contingency import crosstab
+from scipy.stats.contingency import chi2_contingency, association, crosstab
+from scipy.stats.contingency import expected_freq
 from scipy.spatial.distance import squareform
+from scipy.stats import bootstrap
+import pingouin as pg
 
-# libraries for clustering and statistical tests
+# plotting
+import matplotlib.pyplot as plt
+from matplotlib.markers import MarkerStyle
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.ticker import FuncFormatter
+import matplotlib.patches as patches
+import seaborn as sns
+from seaborn.axisgrid import FacetGrid
+import plotly.graph_objects as go
+from statsmodels.graphics.mosaicplot import mosaic
+
+# libraries for clustering
+from sklearn.base import BaseEstimator
+from sklearn.model_selection import ParameterGrid
 import umap
 from sklearn.decomposition import PCA
-from sklearn.cluster import HDBSCAN  
-import pingouin as pg
+import hdbscan
 
 # sequence distance libraries
 # imported in config file!
