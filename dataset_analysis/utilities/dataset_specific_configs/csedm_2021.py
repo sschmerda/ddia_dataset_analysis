9850d09f8efee2a42685a16fa5d394d050fc42c8
Stefan Schmerda <stefan.schmerda@gmail.com>
2024-12-09 18:37:55 +0100

    update config functions

--- dataset_analysis/utilities/dataset_specific_configs/csedm_2021.py
+++ dataset_analysis/utilities/dataset_specific_configs/csedm_2021.py
@@ -8,78 +8,74 @@ from ..result_tables import *
 ### dataset name ###
 ########################################################################################################################
 
-DATASET_NAME = 'kdd_2015'
+DATASET_NAME = 'csedm_2021'
 
 ########################################################################################################################
 ### paths ###
 ########################################################################################################################
 
-PATH_TO_INTERACTION_DATA = '../../../../../../data/ddia/kdd_2015/complete/log_train.csv'
-PATH_TO_ENROLLMENT_DATA = '../../../../../../data/ddia/kdd_2015/complete/enrollment_train.csv'
-PATH_TO_DROPOUT_DATA = '../../../../../../data/ddia/kdd_2015/complete/truth_train.csv'
-PATH_TO_OBJECT_DATA = '../../../../../../data/ddia/kdd_2015/ObjectData/object.csv'
+PATH_TO_INTERACTION_DATA = '../../../../../../data/ddia/CSEDM_2021_F19_Release_All_05_23_22/All/Data/MainTable.csv'
+PATH_TO_COURSE_EVALUATION_DATA = '../../../../../../data/ddia/CSEDM_2021_F19_Release_All_05_23_22/All/Data/LinkTables/Subject.csv'
 
 ########################################################################################################################
 ### required fields ###
 ########################################################################################################################
 
-USER_FIELD = 'username'
-GROUP_FIELD = 'course_id'
-LEARNING_ACTIVITY_FIELD = 'object'
+USER_FIELD = 'SubjectID'
+GROUP_FIELD = 'AssignmentID'
+LEARNING_ACTIVITY_FIELD = 'ProblemID'
 COURSE_FIELD = 'Course'
-TIMESTAMP_FIELD = 'time'
-ORDER_FIELD = None
+TIMESTAMP_FIELD = 'ServerTimestamp'
+ORDER_FIELD = 'Order'
 
 ########################################################################################################################
 ### dataset specific constants ###
 ########################################################################################################################
 
 # fields used in dataset preparation
-ENROLLMENT_FIELD = 'enrollment_id'
-DROPOUT_FIELD = 'is_dropout'
-MODULE_FIELD = 'module_id'
-EVENT_FIELD = 'event'
-CATEGORY_FIELD = 'category'
+EVENT_TYPE_FIELD = 'EventType'
 
 # strings and values used in dataset preparation
-VIDEO_VALUE_STR = 'video'
-PROBLEM_VALUE_STR = 'problem'
+EVENT_TYPE_RUN_PROGRAM_VALUE_STR = 'Run.Program'
+EVENT_TYPE_COMPILE_VALUE_STR = 'Compile'
+EVENT_TYPE_COMPILE_ERROR_VALUE_STR = 'Compile.Error'
 
 ########################################################################################################################
 ### evaluation fields ###
 ########################################################################################################################
 
 # learning activity level
-EVALUATION_LEARNING_ACTIVITY_SCORE_FIELD = None
+EVALUATION_LEARNING_ACTIVITY_SCORE_FIELD = 'Score'
 EVALUATION_LEARNING_ACTIVITY_IS_CORRECT_FIELD = None
 # group level
 EVALUATION_GROUP_SCORE_FIELD = None
-EVALUATION_GROUP_IS_CORRECT_FIELD = 'no_dropout' 
+EVALUATION_GROUP_IS_CORRECT_FIELD = None 
 # course level
-EVALUATION_COURSE_SCORE_FIELD = None
+EVALUATION_COURSE_SCORE_FIELD = 'X-Grade'
 EVALUATION_COURSE_IS_CORRECT_FIELD = None 
 
 ########################################################################################################################
 ### data import and preprocessing config ###
 ########################################################################################################################
 
-INTERACTION_TYPE_VALUES_TO_KEEP_LIST = [PROBLEM_VALUE_STR,
-                                        VIDEO_VALUE_STR]
+INTERACTION_TYPE_VALUES_TO_KEEP_LIST = [EVENT_TYPE_RUN_PROGRAM_VALUE_STR,
+                                        EVENT_TYPE_COMPILE_VALUE_STR,
+                                        EVENT_TYPE_COMPILE_ERROR_VALUE_STR]
 
 DROP_LEARNING_ACTIVITY_SEQUENCE_IF_NA_FIELD_LIST = [TIMESTAMP_FIELD,
                                                     LEARNING_ACTIVITY_FIELD,
-                                                    DROPOUT_FIELD]
+                                                    ORDER_FIELD]
 FIELD_VALUE_TUPLE_NA_FILTER_EXCEPTION_LIST = []
 
 DROP_ROW_IF_NA_FIELD_LIST = [GROUP_FIELD,
                              USER_FIELD]
 
 ########################################################################################################################
-### evaluation fields correct field calculation ###
+### evaluation fields correct field calculation  ###
 ########################################################################################################################
 
 # score threshold is correct
-EVALUATION_LEARNING_ACTIVITY_SCORE_CORRECT_THRESHOLD = None
+EVALUATION_LEARNING_ACTIVITY_SCORE_CORRECT_THRESHOLD = 1
 EVALUATION_GROUP_SCORE_CORRECT_THRESHOLD = None
 EVALUATION_COURSE_SCORE_CORRECT_THRESHOLD = None
 
@@ -99,8 +95,8 @@ EVALUATION_COURSE_SCORE_MAXIMUM_IN_DATASET_DOCU = None
 ### evaluation field for omnibus test per cluster per group ###
 ########################################################################################################################
 
-OMNIBUS_TESTS_EVAlUATION_METRIC_NAME_STR = EVALUATION_GROUP_IS_CORRECT_FIELD_NAME_STR
-OMNIBUS_TESTS_EVAlUATION_METRIC_IS_CATEGORICAL_NAME_STR = True
+OMNIBUS_TESTS_EVAlUATION_METRIC_NAME_STR = EVALUATION_GROUP_SCORE_HIGHEST_ALL_LEARNING_ACTIVITIES_MEAN_FIELD_NAME_STR
+OMNIBUS_TESTS_EVAlUATION_METRIC_IS_CATEGORICAL_NAME_STR = False
 
 ########################################################################################################################
 ### pickle paths and file names ###
@@ -158,7 +154,12 @@ result_tables = ResultTables(DATASET_NAME,
 ########################################################################################################################
 
 # calculate_eval_metrics #
-def calculate_eval_metrics(df: pd.DataFrame):
+def calculate_eval_metrics(df: pd.DataFrame,
+                           evaluation_score_field=None,
+                           evaluation_score_correct_threshold=None,
+                           event_type_field=None,
+                           event_type_run_program_value_str=None,
+                           server_timestamp_field=None):
     """Calculate evaluation metrics per single learning_activity. This function should be used as input of the
     add_evaluation_fields function.
 
@@ -166,6 +167,16 @@ def calculate_eval_metrics(df: pd.DataFrame):
     ----------
     df : pd.DataFrame
         A subset of the interactions dataframe containing data of a single learning_activity
+    evaluation_score_field : _type_
+        The learning_activity evaluation score field column, by default None
+    evaluation_score_correct_threshold : _type_
+        The threshold for the learning_activity score values to be evaluated as correct, by default None
+    event_type_field : _type_, optional
+        The event_type field column, by default None
+    event_type_run_program_value_str : _type_
+        The run_program value of the event_type field, by default None
+    server_timestamp_field : _type_
+        The server_timestamp_field field, by default None
 
     Returns
     -------
@@ -174,17 +185,23 @@ def calculate_eval_metrics(df: pd.DataFrame):
     """
     df = df.copy()
 
-    # number interactions
-    number_interactions_total = df.shape[0]
+    # generate corrects from score
+    evaluation_correct_field = 'correct'
+    df[evaluation_correct_field] = df[evaluation_score_field].apply(lambda x: float(x) if pd.notna(x) else x) >= evaluation_score_correct_threshold
 
-    # number attempts (needs to be the same as interactions)
-    number_attempts_total = df.shape[0]
+    # number interactions (Run.Program)
+    number_interactions_total = sum(df[event_type_field]==event_type_run_program_value_str)
+
+    # number attempts
+    number_attempts_total = sum(df[event_type_field]==event_type_run_program_value_str)
 
     # number hints
     number_hints_total = None
 
     # total time
-    time_total = None
+    # (timedelta between first and last run_program interaction, can be 0 for only 1 attempt)
+    run_program_df = df.loc[df[event_type_field]==event_type_run_program_value_str, :]
+    time_total = round((run_program_df[server_timestamp_field].iloc[-1] - run_program_df[server_timestamp_field].iloc[0]).total_seconds())
         
     # scores
     # single score
@@ -194,43 +211,62 @@ def calculate_eval_metrics(df: pd.DataFrame):
     # single score not first attempt lowest
     single_score_not_first_attempt_lowest = None
     # highest score
-    score_highest = None
+    score_highest = df[evaluation_score_field].max()
     # highest score without hint
     score_highest_without_hint = None
     # score first attempt
-    score_first_attempt = None
+    score_first_attempt = df[evaluation_score_field][df[event_type_field]==event_type_run_program_value_str].to_list()[0]
     # score last attempt
-    score_last_attempt = None
+    score_last_attempt = df[evaluation_score_field][df[event_type_field]==event_type_run_program_value_str].to_list()[-1]
     # number interactions until highest score
-    number_interactions_until_score_highest = None
+    number_interactions_until_score_highest = np.argmax(df[evaluation_score_field][df[event_type_field]==event_type_run_program_value_str]) + 1
     # number attempts until highest score
-    number_attempts_until_score_highest = None
+    number_attempts_until_score_highest = np.argmax(df[evaluation_score_field][df[event_type_field]==event_type_run_program_value_str]) + 1
     # number hints until highest score
     number_hints_until_score_highest = None
     # time until highest score
-    time_until_score_highest = None
+    # (timedelta between first run_program interaction and highest score run_program interaction, can be 0 for only 1 attempt)
+    index_highest_score = np.argmax(run_program_df[evaluation_score_field])
+    time_until_score_highest = round((run_program_df[server_timestamp_field].iloc[index_highest_score] - run_program_df[server_timestamp_field].iloc[0]).total_seconds())
 
     # corrects
     # correct
-    is_correct = None
+    is_correct = df[evaluation_correct_field].any()
     # correct without hint
     is_correct_without_hint = None
     # correct first attempt
-    is_correct_first_attempt = None
+    try:
+        is_correct_first_attempt = df[evaluation_correct_field][df[event_type_field]==event_type_run_program_value_str].to_list()[0]
+    except:
+        is_correct_first_attempt = False
     # correct first attempt without hint
     is_correct_first_attempt_without_hint = None
     # correct last attempt
-    is_correct_last_attempt = None
+    try:
+        is_correct_last_attempt = df[evaluation_correct_field][df[event_type_field]==event_type_run_program_value_str].to_list()[-1]
+    except:
+        is_correct_last_attempt = False
     # correct last attempt without hint
     is_correct_last_attempt_without_hint = None
     # number interactions until correct
-    number_interactions_until_correct = None
+    if is_correct:
+        number_interactions_until_correct = np.argmax(df[evaluation_correct_field][df[event_type_field]==event_type_run_program_value_str]) + 1
+    else:
+        number_interactions_until_correct = -1 # -1 means that there was no correct -> therefore also no interactions until correct
     # number attempts until correct
-    number_attempts_until_correct = None
+    if is_correct:
+        number_attempts_until_correct = np.argmax(df[evaluation_correct_field][df[event_type_field]==event_type_run_program_value_str]) + 1
+    else:
+        number_attempts_until_correct = -1 # -1 means that there was no correct -> therefore also no attempts until correct
     # number hints until correct
     number_hints_until_correct = None
     # time until correct
-    time_until_correct = None
+    # (timedelta between first run_program interaction and first is_correct run_program interaction, can be 0 for only 1 attempt)
+    if is_correct:
+        index_is_correct = np.argmax(run_program_df[evaluation_correct_field])
+        time_until_correct = round((run_program_df[server_timestamp_field].iloc[index_is_correct] - run_program_df[server_timestamp_field].iloc[0]).total_seconds())
+    else:
+        time_until_correct = -1 # -1 means that there was no correct -> therefore also no attempts until correct
 
     return (number_interactions_total,
             number_attempts_total,
@@ -258,8 +294,8 @@ def calculate_eval_metrics(df: pd.DataFrame):
             number_hints_until_correct,
             time_until_correct)
 
-#kdd_2015 specific preprocessing functions
-class ImportInteractionsKDD2015(ImportInteractions):
+#csedm_2021 specific preprocessing functions
+class ImportInteractionsCSEDM2021(ImportInteractions):
     """Imports the interaction dataframe and filters it by interactions of a specified type. 
 
     Attributes
@@ -273,37 +309,28 @@ class ImportInteractionsKDD2015(ImportInteractions):
     def _create_interactions_df(self) -> pd.DataFrame:
 
         interactions = pd.read_csv(PATH_TO_INTERACTION_DATA)
-        enrollments = pd.read_csv(PATH_TO_ENROLLMENT_DATA)
-        dropouts = pd.read_csv(PATH_TO_DROPOUT_DATA, names=[ENROLLMENT_FIELD, DROPOUT_FIELD])
-        objects = pd.read_csv(PATH_TO_OBJECT_DATA) 
+        course_grade = pd.read_csv(PATH_TO_COURSE_EVALUATION_DATA)
 
         interactions = typecast_fields(interactions,
                                        TIMESTAMP_FIELD,
-                                       None,
-                                       None,
+                                       GROUP_FIELD,
+                                       USER_FIELD,
                                        LEARNING_ACTIVITY_FIELD)
 
-        enrollments = typecast_fields(enrollments,
-                                      None,
-                                      GROUP_FIELD,
-                                      USER_FIELD,
-                                      None)
+        course_grade = typecast_fields(course_grade,
+                                       None,
+                                       None,
+                                       USER_FIELD,
+                                       None)
 
-        objects = objects[[MODULE_FIELD, CATEGORY_FIELD]].rename(columns={MODULE_FIELD: LEARNING_ACTIVITY_FIELD})
-        objects = objects.loc[~objects.duplicated(), :]
-
-        interactions = pd.merge(interactions, enrollments, how='inner', on=ENROLLMENT_FIELD)
-        interactions = pd.merge(interactions, dropouts, how='inner', on=ENROLLMENT_FIELD)
-        interactions = interactions.merge(objects, how='left', on=LEARNING_ACTIVITY_FIELD)
+        interactions = interactions.merge(course_grade, how="left", on=USER_FIELD)
 
         return interactions
 
-class TransformInteractionsKDD2015(TransformInteractions):
-     
+class TransformInteractionsCSEDM2021(TransformInteractions):
+
     def _transform_interactions_df(self,
                                    interactions: pd.DataFrame) -> pd.DataFrame:
-
-        interactions[EVALUATION_GROUP_IS_CORRECT_FIELD] = interactions[DROPOUT_FIELD].map({0: True, 1: False})
 
         return interactions
     
